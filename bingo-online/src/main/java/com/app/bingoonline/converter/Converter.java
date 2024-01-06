7f7b409b411ae625f18a69c6f82c3029d38ef7d5
Andre <parelhoneves.andre@gmail.com>
2024-01-05 21:54:55 -0300

    feat: add new method to converter

--- bingo-online/src/main/java/com/app/bingoonline/converter/Converter.java
+++ bingo-online/src/main/java/com/app/bingoonline/converter/Converter.java
@@ -4,13 +4,33 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Component;
 
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Component
 public class Converter {
     public String mapToJson(Map<String, Set<Integer>> map) throws JsonProcessingException {
         ObjectMapper objectMapper = new ObjectMapper();
         return objectMapper.writeValueAsString(map);
+    }
+
+    public List<Integer> convertStringToList(String data){
+        List<Integer> numbers = new ArrayList<>();
+
+        StringTokenizer tokenizer = new StringTokenizer(data, ",");
+        int tokenCount = tokenizer.countTokens();
+
+        String[] stringArray = new String[tokenCount];
+
+        for (int i = 0; i < tokenCount; i++) {
+            stringArray[i] = tokenizer.nextToken();
+            numbers.add(Integer.valueOf(stringArray[i]));
+        }
+
+        return numbers;
+    }
+
+    public String convertListToString(List<String> integerList){
+        return integerList.stream().map(String::valueOf).collect(Collectors.joining(","));
     }
 }
