fe205aa36aa81c84123e55105b6c17d455fa3700
Andre <parelhoneves.andre@gmail.com>
2024-01-05 21:53:32 -0300

    test: add shared

---
+++ bingo-online/src/test/java/com/app/bingoonline/shared/TicketStub.java
@@ -0,0 +1,36 @@
+package com.app.bingoonline.shared;
+
+import com.app.bingoonline.model.extds.*;
+
+import java.util.*;
+
+public class TicketStub {
+    public B b = new B();
+    public I i = new I();
+    public N n = new N();
+    public G g = new G();
+    public O o = new O();
+
+    public Map<String, Set<Integer>> generateCardTicket(){
+        Map<String, Set<Integer>> ticket = new HashMap<>();
+        ticket.put("b", this.b.generateTicketNumbers());
+        ticket.put("i", this.i.generateTicketNumbers());
+        ticket.put("n", this.n.generateTicketNumbers());
+        ticket.put("g", this.g.generateTicketNumbers());
+        ticket.put("o", this.o.generateTicketNumbers());
+        return  ticket;
+    }
+
+    public Map<String, String> convertStringToMap(String data) {
+        Map<String, String> map = new HashMap<>();
+        StringTokenizer tokenizer = new StringTokenizer(data, " ");
+
+        while (tokenizer.hasMoreTokens()) {
+            String token = tokenizer.nextToken();
+            String[] keyValue = token.split("=");
+            map.put(keyValue[0], keyValue[1]);
+        }
+
+        return map;
+    }
+}
