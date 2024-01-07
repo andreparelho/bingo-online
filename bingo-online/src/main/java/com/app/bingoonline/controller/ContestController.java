dee683ed0c2c11f85cebf9a5c3a379e3fa634c6b
Andre <parelhoneves.andre@gmail.com>
2024-01-06 21:27:54 -0300

    update controller

--- bingo-online/src/main/java/com/app/bingoonline/controller/ContestController.java
+++ bingo-online/src/main/java/com/app/bingoonline/controller/ContestController.java
@@ -1,5 +1,6 @@
 package com.app.bingoonline.controller;
 
+import com.app.bingoonline.entity.TicketEntity;
 import com.app.bingoonline.service.ContestService;
 import com.app.bingoonline.service.TicketService;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -33,19 +34,16 @@ public class ContestController {
 
     @PostMapping("/{contest-number}/tickets")
     public ResponseEntity<Map<String, Set<Integer>>> createTicket(@PathVariable("contest-number") int contestNumber) throws Exception {
-        Map<String, Set<Integer>> tickets = this.ticketService.generateTicketByContestId(contestNumber);
-        return ResponseEntity.ok(tickets);
+        Map<String, Set<Integer>> newTicket = this.ticketService.generateTicketByContestId(contestNumber);
+        return ResponseEntity.ok(newTicket);
     }
 
     @GetMapping("/{contest-number}/tickets")
-    public ResponseEntity<Map<Integer, List<String>>> getAllTicketsByContest(@PathVariable("contest-number") int contestNumber) throws Exception {
+    public ResponseEntity<Map<Integer, List<TicketEntity>>> getAllTicketsByContest(@PathVariable("contest-number") int contestNumber) throws Exception {
        /* CRIAR UMA SERVICE PARA TRAZER TODOS OS TICKETS BASEADO NO ID DO CONTEST, A IDEIA INICAL E UM MAP COM NUMERO DO CONTEST E UMA LISTA DE TICKETS */
-        Map<Integer, List<String>> allTickets = new HashMap<>();
-        List<String> tickets = new ArrayList<>();
-        tickets.add("{\"b\":[2,6,9,12,14],\"g\":[48,51,59,46,47],\"i\":[21,22,25,28,29],\"Contest\":[9867],\"n\":[37,38,42,31],\"o\":[65,71,72,74,61]}");
-        tickets.add("{\"b\":[1,9,11,12,13],\"g\":[48,49,52,55,58],\"i\":[16,18,21,24,25],\"Contest\":[1151],\"n\":[32,36,39,40],\"o\":[65,66,72,73,74]}");
-        tickets.add("{\"b\":[7,10,11,12,14],\"g\":[52,54,55,58,46],\"i\":[16,18,19,23,25],\"Contest\":[1832],\"n\":[38,41,43,31],\"o\":[64,65,69,72,62]}");
-        allTickets.put(contestNumber, tickets);
+
+        Map<Integer, List<TicketEntity>> allTickets = this.ticketService.getAllTicketsByContest(contestNumber);
+
         return ResponseEntity.ok(allTickets);
     }
 
