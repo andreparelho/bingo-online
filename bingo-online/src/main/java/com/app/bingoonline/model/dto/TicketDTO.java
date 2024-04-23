65766f4104f63084134c952fa6f12e7a70856e65
Andre Parelho <parelhoneves.andre@gmail.com>
2024-04-23 20:36:18 -0300

    rename model to dto

--- bingo-online/src/main/java/com/app/bingoonline/model/dto/TicketDTO.java
+++ bingo-online/src/main/java/com/app/bingoonline/model/dto/TicketDTO.java
@@ -1,11 +1,20 @@
-package com.app.bingoonline.model;
+package com.app.bingoonline.model.dto;
 
 import lombok.Builder;
 
 @Builder
-public class TicketModel {
+public class TicketDTO {
+    private Long id;
     private Long contestNumberId;
     private String ticket;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public Long getContestNumberId() {
         return contestNumberId;
