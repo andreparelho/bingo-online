c7e890f0241c199ac7dfc051846f9d5416ea80d4
Andre Parelho <parelhoneves.andre@gmail.com>
2024-04-23 20:36:40 -0300

    update mappers

--- bingo-online/src/main/java/com/app/bingoonline/mapper/ContestMapper.java
+++ bingo-online/src/main/java/com/app/bingoonline/mapper/ContestMapper.java
@@ -1,22 +1,23 @@
 package com.app.bingoonline.mapper;
 
 import com.app.bingoonline.entity.ContestEntity;
-import com.app.bingoonline.model.ContestModel;
+import com.app.bingoonline.model.dto.ContestDTO;
 
 public class ContestMapper {
-    public ContestModel entityToModel(ContestEntity contestEntity){
-        return ContestModel.builder()
+    public ContestDTO entityToDTO(ContestEntity contestEntity){
+        return ContestDTO.builder()
+                .id(contestEntity.getId())
                 .raffleNumbers(contestEntity.getRaffleNumbers())
                 .number(contestEntity.getNumber())
                 .contestNumber(contestEntity.getContestNumber())
                 .build();
     }
 
-    public ContestEntity modelToEntity(ContestModel contestModel){
+    public ContestEntity DTOToEntity(ContestDTO contestDTO){
         return ContestEntity.builder()
-                .raffleNumbers(contestModel.getRaffleNumbers())
-                .number(contestModel.getNumber())
-                .contestNumber(contestModel.getContestNumber())
+                .raffleNumbers(contestDTO.getRaffleNumbers())
+                .number(contestDTO.getNumber())
+                .contestNumber(contestDTO.getContestNumber())
                 .build();
     }
 }
--- bingo-online/src/main/java/com/app/bingoonline/mapper/TicketMapper.java
+++ bingo-online/src/main/java/com/app/bingoonline/mapper/TicketMapper.java
@@ -1,22 +1,21 @@
 package com.app.bingoonline.mapper;
 
-import com.app.bingoonline.entity.ContestEntity;
 import com.app.bingoonline.entity.TicketEntity;
-import com.app.bingoonline.model.ContestModel;
-import com.app.bingoonline.model.TicketModel;
+import com.app.bingoonline.model.dto.TicketDTO;
 
 public class TicketMapper {
-    public TicketModel entityToModel(TicketEntity ticketEntity){
-        return TicketModel.builder()
+    public TicketDTO entityToDTO(TicketEntity ticketEntity){
+        return TicketDTO.builder()
+                .id(ticketEntity.getId())
                 .ticket(ticketEntity.getTicket())
                 .contestNumberId(ticketEntity.getContestNumberId())
                 .build();
     }
 
-    public TicketEntity modelToEntity(TicketModel ticketModel){
+    public TicketEntity DTOToEntity(TicketDTO ticketDTO){
         return TicketEntity.builder()
-                .ticket(ticketModel.getTicket())
-                .contestNumberId(ticketModel.getContestNumberId())
+                .ticket(ticketDTO.getTicket())
+                .contestNumberId(ticketDTO.getContestNumberId())
                 .build();
     }
 }
