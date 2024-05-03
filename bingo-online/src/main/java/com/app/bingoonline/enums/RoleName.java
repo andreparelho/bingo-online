98d67bd6a5b66b3a4f973d0f02d15342025dc1ec
Andre Parelho <parelhoneves.andre@gmail.com>
2024-05-02 21:58:07 -0300

    new packages and classes

---
+++ bingo-online/src/main/java/com/app/bingoonline/enums/RoleName.java
@@ -0,0 +1,6 @@
+package com.app.bingoonline.enums;
+
+public enum RoleName {
+    ROLE_SELLER,
+    ROLE_ADMINISTRATOR
+}
---
+++ bingo-online/src/main/java/com/app/bingoonline/model/request/CreateUserRequest.java
@@ -0,0 +1,4 @@
+package com.app.bingoonline.model.request;
+
+public class CreateUserRequest {
+}
