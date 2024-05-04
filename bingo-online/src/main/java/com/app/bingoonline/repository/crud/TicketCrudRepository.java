36e847a2ed6f56e37133ec93fb9ab91de4185689
Andre Parelho <parelhoneves.andre@gmail.com>
2024-05-04 19:55:02 -0300

    refact: refactoring repositories

--- bingo-online/src/main/java/com/app/bingoonline/repository/crud/TicketCrudRepository.java
+++ bingo-online/src/main/java/com/app/bingoonline/repository/crud/TicketCrudRepository.java
@@ -4,7 +4,8 @@ import com.app.bingoonline.entity.TicketEntity;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.UUID;
 
-public interface TicketCrudRepository extends CrudRepository<TicketEntity, Long> {
+public interface TicketCrudRepository extends CrudRepository<TicketEntity, UUID> {
     List<TicketEntity> findByContestNumberId(long contestNumberId);
 }
