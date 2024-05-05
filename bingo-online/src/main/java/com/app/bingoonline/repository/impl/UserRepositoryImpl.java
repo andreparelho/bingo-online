6fd416e6a15d918853faebb374fc5202dbef3046
Andre Parelho <parelhoneves.andre@gmail.com>
2024-05-05 01:37:06 -0300

    feat: add annotation to fix build

--- bingo-online/src/main/java/com/app/bingoonline/repository/impl/UserRepositoryImpl.java
+++ bingo-online/src/main/java/com/app/bingoonline/repository/impl/UserRepositoryImpl.java
@@ -2,9 +2,11 @@ package com.app.bingoonline.repository.impl;
 
 import com.app.bingoonline.entity.UserEntity;
 import com.app.bingoonline.repository.UserRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Service
 public class UserRepositoryImpl implements UserRepository {
     @Override
     public Optional<UserEntity> findByUsername(String username) {
