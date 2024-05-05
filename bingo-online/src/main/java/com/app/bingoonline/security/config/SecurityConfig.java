806637e9fe619384e96990af81422b11da4d213e
Andre Parelho <parelhoneves.andre@gmail.com>
2024-05-05 20:44:52 -0300

    feat: update securities configs

--- bingo-online/src/main/java/com/app/bingoonline/security/config/SecurityConfig.java
+++ bingo-online/src/main/java/com/app/bingoonline/security/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.app.bingoonline.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,7 +14,10 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+        httpSecurity.authorizeHttpRequests(auth ->
+                        auth
+                                .requestMatchers(HttpMethod.POST, "/v1/login").permitAll()
+                                .anyRequest().authenticated())
                 .csrf(csrf -> csrf.disable())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
