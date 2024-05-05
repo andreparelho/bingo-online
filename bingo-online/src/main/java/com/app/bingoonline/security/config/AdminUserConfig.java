e96b0dbcc37a93d795fcb9d71b9edb652649edc7
Andre Parelho <parelhoneves.andre@gmail.com>
2024-05-05 20:45:20 -0300

    feat: create admin config to build with user

---
+++ bingo-online/src/main/java/com/app/bingoonline/security/config/AdminUserConfig.java
@@ -0,0 +1,69 @@
+package com.app.bingoonline.security.config;
+
+import com.app.bingoonline.entity.RoleEntity;
+import com.app.bingoonline.entity.UserEntity;
+import com.app.bingoonline.repository.RoleRepository;
+import com.app.bingoonline.repository.UserRepository;
+import com.app.bingoonline.repository.crud.RoleCrudRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Role;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Configuration
+public class AdminUserConfig implements CommandLineRunner {
+    private RoleRepository roleRepository;
+    private RoleCrudRepository roleCrudRepository;
+    private UserRepository userRepository;
+    private BCryptPasswordEncoder passwordEncoder;
+
+    public AdminUserConfig(RoleRepository roleRepository, RoleCrudRepository roleCrudRepository,
+                           UserRepository userRepository,
+                           BCryptPasswordEncoder passwordEncoder) {
+        this.roleRepository = roleRepository;
+        this.roleCrudRepository = roleCrudRepository;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    @Transactional
+    public void run(String... args) throws Exception {
+
+        // Criar a função de administrador se não existir
+        RoleEntity adminRole = roleCrudRepository.findByName(RoleEntity.Values.ADMIN.name());
+        if (adminRole == null) {
+            adminRole = RoleEntity.builder()
+                    .name(RoleEntity.Values.ADMIN.name())
+                    .build();
+            roleCrudRepository.save(adminRole);
+            System.out.println("role admin created" + "  " + roleCrudRepository.save(adminRole));
+        }
+
+        // Criar a função de vendedor se não existir
+        RoleEntity sellerRole = roleCrudRepository.findByName(RoleEntity.Values.BASIC.name());
+        if (sellerRole == null) {
+            sellerRole = RoleEntity.builder()
+                        .name(RoleEntity.Values.BASIC.name())
+                    .build();
+            roleCrudRepository.save(sellerRole);
+            System.out.println("role seller created");
+        }
+
+        // Agora você pode criar o usuário com as funções criadas ou existentes
+        UserEntity user = UserEntity.builder()
+                .username("admin")
+                .password(passwordEncoder.encode("123"))
+                .roles(Set.of(adminRole))
+                .build();
+
+        userRepository.save(user);
+        System.out.println("user created");
+    }
+
+}
