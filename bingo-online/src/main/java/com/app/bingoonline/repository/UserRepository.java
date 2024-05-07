cd3f87017fd8a39856c7aba084e11063778ba7f5
Andre Parelho <parelhoneves.andre@gmail.com>
2024-05-07 00:23:10 -0300

    feat: create new methods

--- bingo-online/src/main/java/com/app/bingoonline/repository/UserRepository.java
+++ bingo-online/src/main/java/com/app/bingoonline/repository/UserRepository.java
@@ -1,12 +1,16 @@
 package com.app.bingoonline.repository;
 
 import com.app.bingoonline.entity.UserEntity;
+import com.app.bingoonline.model.request.UserUpdateRequest;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
 public interface UserRepository {
     Optional<UserEntity> findByUsername(String username);
     UserEntity save(UserEntity userEntity);
+    UserEntity updateUser(UserEntity userEntity);
+    Optional<UserEntity> findById(UUID id);
 }
