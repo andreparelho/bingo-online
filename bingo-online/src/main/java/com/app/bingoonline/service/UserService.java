1c9ba4994ed91b88b0b6b0ead63c6c10d5335d61
Andre Parelho <parelhoneves.andre@gmail.com>
2024-05-07 00:22:36 -0300

    feat: create new methods and refact

--- bingo-online/src/main/java/com/app/bingoonline/service/UserService.java
+++ bingo-online/src/main/java/com/app/bingoonline/service/UserService.java
@@ -1,8 +1,15 @@
 package com.app.bingoonline.service;
 
+import com.app.bingoonline.entity.UserEntity;
 import com.app.bingoonline.model.request.CreateUserRequest;
+import com.app.bingoonline.model.request.UserUpdateRequest;
+
+import java.util.Optional;
+import java.util.UUID;
 
 
 public interface UserService {
     void createUser(CreateUserRequest createUserRequest);
+    void updateUser(UUID id, UserUpdateRequest userUpdateRequest);
+    Optional<UserEntity> findUserById(UUID uuid);
 }
