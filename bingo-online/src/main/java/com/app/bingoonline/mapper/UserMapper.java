3ce382b16ec0a08f6912244dd5cd4ef9f5562aab
Andre Parelho <parelhoneves.andre@gmail.com>
2024-05-06 21:15:49 -0300

    feat: implement mapper to user

--- bingo-online/src/main/java/com/app/bingoonline/mapper/UserMapper.java
+++ bingo-online/src/main/java/com/app/bingoonline/mapper/UserMapper.java
@@ -1,4 +1,15 @@
 package com.app.bingoonline.mapper;
 
+import com.app.bingoonline.entity.UserEntity;
+import com.app.bingoonline.model.request.CreateUserRequest;
+import org.springframework.stereotype.Component;
+
+@Component
 public class UserMapper {
+    public UserEntity dtoToEntity(CreateUserRequest createUserRequest){
+        return UserEntity.builder()
+                .username(createUserRequest.username())
+                .password(createUserRequest.password())
+                .build();
+    }
 }
