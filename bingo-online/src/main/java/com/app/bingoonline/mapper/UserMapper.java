602ddca665aac8f38e1c79c9ffd6c332e8bea28a
Andre Parelho <parelhoneves.andre@gmail.com>
2024-05-07 00:24:34 -0300

    create mapper to dto

--- bingo-online/src/main/java/com/app/bingoonline/mapper/UserMapper.java
+++ bingo-online/src/main/java/com/app/bingoonline/mapper/UserMapper.java
@@ -2,6 +2,7 @@ package com.app.bingoonline.mapper;
 
 import com.app.bingoonline.entity.UserEntity;
 import com.app.bingoonline.model.request.CreateUserRequest;
+import com.app.bingoonline.model.request.UserUpdateRequest;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,6 +11,13 @@ public class UserMapper {
         return UserEntity.builder()
                 .username(createUserRequest.username())
                 .password(createUserRequest.password())
+                .build();
+    }
+
+    public UserEntity dtoToEntity(UserUpdateRequest userUpdateRequest){
+        return UserEntity.builder()
+                .username(userUpdateRequest.username())
+                .password(userUpdateRequest.password())
                 .build();
     }
 }
