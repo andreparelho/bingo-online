8257f247fe7de007805510ce52bdfa9fc2e9f59b
Andre Parelho <parelhoneves.andre@gmail.com>
2024-05-07 00:23:56 -0300

    implementing update user

--- bingo-online/src/main/java/com/app/bingoonline/controller/UserController.java
+++ bingo-online/src/main/java/com/app/bingoonline/controller/UserController.java
@@ -1,32 +1,53 @@
 package com.app.bingoonline.controller;
 
+import com.app.bingoonline.entity.UserEntity;
+import com.app.bingoonline.exception.user.UserNotFoundException;
 import com.app.bingoonline.model.request.CreateUserRequest;
+import com.app.bingoonline.model.request.UserUpdateRequest;
+import com.app.bingoonline.service.JwtService;
 import com.app.bingoonline.service.UserService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/v1/users")
 public class UserController {
-
     private final UserService userService;
+    private final JwtService jwtService;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService, JwtService jwtService) {
         this.userService = userService;
+        this.jwtService = jwtService;
     }
 
     @PostMapping
     public ResponseEntity<Void> createUser(@RequestBody CreateUserRequest createUserRequest){
         this.userService.createUser(createUserRequest);
-        return ResponseEntity.status(201).build();
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @GetMapping("/user-by-admin")
     public void getUserByAdmin(){
     }
 
-    @PutMapping
-    public void updateUser(){
+    @PutMapping("/update")
+    public ResponseEntity<Void> updateUser(@RequestBody UserUpdateRequest userUpdateRequest,
+                                           JwtAuthenticationToken authorizationHeader){
+
+        Optional<UserEntity> user = this.userService.findUserById(UUID.fromString(authorizationHeader.getName()));
+
+        if (user.isEmpty()){
+            throw new UserNotFoundException("user not found");
+        }
+
+        this.userService.updateUser(UUID.fromString(authorizationHeader.getName()), userUpdateRequest);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
     @DeleteMapping
