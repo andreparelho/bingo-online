8da988d643c4ad035639e223f5d2c40377ad4933
Andre Parelho <parelhoneves.andre@gmail.com>
2024-05-06 21:11:57 -0300

    feat: create logs on controller

--- bingo-online/src/main/java/com/app/bingoonline/controller/LoginController.java
+++ bingo-online/src/main/java/com/app/bingoonline/controller/LoginController.java
@@ -2,10 +2,9 @@ package com.app.bingoonline.controller;
 
 import com.app.bingoonline.model.request.LoginRequest;
 import com.app.bingoonline.model.response.LoginResponse;
-import com.app.bingoonline.repository.UserRepository;
 import com.app.bingoonline.service.LoginService;
+import com.app.bingoonline.util.LogUtil;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,8 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/v1/login")
 public class LoginController {
-
     private final LoginService loginService;
+    private static final LogUtil logger = new LogUtil();
 
     public LoginController(LoginService loginService) {
         this.loginService = loginService;
@@ -23,7 +22,12 @@ public class LoginController {
 
     @PostMapping
     public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest){
-        return this.loginService.login(loginRequest);
+        logger.createLog(getClass().getName(), "login", "login user request", null);
+
+        LoginResponse response = this.loginService.login(loginRequest);
+
+        logger.createLog("login", "login success", response);
+        return ResponseEntity.ok(response);
     }
 
     @PostMapping("/logout")
