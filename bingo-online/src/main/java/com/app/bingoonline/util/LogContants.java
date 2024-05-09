618422f78c555282889080b9d51c6fe88234611c
Andre Parelho <parelhoneves.andre@gmail.com>
2024-05-08 22:59:52 -0300

    feat: create constant class

---
+++ bingo-online/src/main/java/com/app/bingoonline/util/LogContants.java
@@ -0,0 +1,45 @@
+package com.app.bingoonline.util;
+
+public class LogContants {
+//    Class Names
+    public static final String CONFIG_CLASS = "Config Class";
+    public static final String CONTEST_CONTROLLER_CLASS = "Contest Controller";
+    public static final String LOGIN_CONTROLLER_CLASS = "Login Controller";
+    public static final String USER_CONTROLLER_CLASS = "User Controller";
+    public static final String CONTEST_SERVICE_CLASS = "Contest Service";
+    public static final String USER_SERVICE_CLASS = "User Service";
+    public static final String LOGIN_SERVICE_CLASS = "Login Service";
+    public static final String JWT_SERVICE_CLASS = "Jwt Service";
+    public static final String TICKET_SERVICE_CLASS = "Ticket Service";
+    public static final String CONTEST_REPOSITORY_CLASS = "Contest Repository";
+    public static final String ROLE_REPOSITORY_CLASS = "Role Repository";
+    public static final String USER_REPOSITORY_CLASS = "User Repository";
+    public static final String TICKET_REPOSITORY_CLASS = "Ticket Repository";
+    public static final String CONVERTER_CLASS = "Converter";
+    public static final String SEED_CLASS = "Seed";
+    public static final String CONTEST_MAPPER_CLASS = "Contest Mapper";
+    public static final String TICKET_MAPPER_CLASS = "Ticket Mapper";
+    public static final String USER_MAPPER_CLASS = "User Mapper";
+
+//    Log Types
+    public static final String CONTROLLER_TYPE = "controller";
+    public static final String SERVICE_TYPE = "service";
+    public static final String REPOSITORY_TYPE = "repository";
+    public static final String MAPPER_TYPE = "mapper";
+    public static final String CONFIG_TYPE = "config";
+    public static final String ERROR_TYPE = "error";
+
+//    Method Names
+    public static final String RANDOM_METHOD = "random";
+    public static final String CONTEST_GET_ALL_METHOD = "getAllContests";
+    public static final String CONTEST_CREATE_METHOD = "createContest";
+    public static final String CONTEST_CREATE_TICKET_METHOD = "createTicket";
+    public static final String CONTEST_GET_ALL_TICKETS_METHOD = "getAllTicketsByContest";
+    public static final String CONTEST_GET_RAFFLE_METHOD = "getRaffleNumber";
+
+//    Log Messages
+    public static final String RANDOM_METHOD_MSG = "creating random bean";
+    public static final String CONTEST_INIT_METHOD_MSG = "initializing controller";
+    public static final String CONTEST_FIN_METHOD_MSG = "finalizing controller";
+
+}
