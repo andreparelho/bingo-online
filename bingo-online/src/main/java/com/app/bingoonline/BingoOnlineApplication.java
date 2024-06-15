9360739f35f3c0188a3c35d3911e7ea70102dfc4
Andre Parelho <parelhoneves.andre@gmail.com>
2024-06-14 23:44:41 -0300

    refactor main

--- bingo-online/src/main/java/com/app/bingoonline/BingoOnlineApplication.java
+++ bingo-online/src/main/java/com/app/bingoonline/BingoOnlineApplication.java
@@ -7,20 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = {
-		"com.app.bingoonline.contest.repository", "com.app.bingoonline.infrastructure.db",
-		"com.app.bingoonline.raffle.repository", "com.app.bingoonline.ticket.repository",
-		"com.app.bingoonline.user.repository"
-})
-@EntityScan(basePackages = {
-		"com.app.bingoonline.contest.entity", "com.app.bingoonline.raffle.entity",
-		"com.app.bingoonline.ticket.entity", "com.app.bingoonline.user.entity"
-})
-@ComponentScan(basePackages = {
-		"com.app.bingoonline.infrastructure.util.mapper.Mapper", "com.app.bingoonline"
-})
 public class BingoOnlineApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BingoOnlineApplication.class, args);
 	}
