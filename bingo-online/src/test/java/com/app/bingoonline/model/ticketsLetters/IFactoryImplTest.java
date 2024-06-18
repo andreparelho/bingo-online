de6a63ae35c8add76b0386be5c7e67990286c578
Andre Parelho <parelhoneves.andre@gmail.com>
2024-06-18 20:47:24 -0300

    test: fix

--- bingo-online/src/test/java/com/app/bingoonline/model/ticketsLetters/IFactoryImplTest.java
+++ bingo-online/src/test/java/com/app/bingoonline/model/ticketsLetters/IFactoryImplTest.java
@@ -4,6 +4,8 @@ import com.app.bingoonline.ticket.factory.impl.IFactoryImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
 import java.util.Set;
@@ -11,6 +13,7 @@ import java.util.Set;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@ExtendWith(SpringExtension.class)
 public class IFactoryImplTest {
 
     private IFactoryImpl letterIFactoryImpl;
